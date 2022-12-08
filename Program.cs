5eac7f02c33fb6a6ae18746f56d4860ada9013e9
ParmesanLinguin <40549567+ParmesanLinguin@users.noreply.github.com>
2022-12-08 17:54:49 -0500

    Day 8

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿//Console.WriteLine($"Solutions: ");
-//Console.WriteLine($"Day 1:\n  Part 1: {Day1.Part1()}\n  Part 2: {Day1.Part2()}\n");
-//Console.WriteLine($"Day 2:\n  Part 1: {Day2.Part1()}\n  Part 2: {Day2.Part2()}\n");
-//Console.WriteLine($"Day 3:\n  Part 1: {Day3.Part1()}\n  Part 2: {Day3.Part2()}\n");
-//Console.WriteLine($"Day 4:\n  Part 1: {Day4.Part1()}\n  Part 2: {Day4.Part2()}\n");
-//Console.WriteLine($"Day 5:\n  Part 1: {Day5.Part1()}\n  Part 2: {Day5.Part2()}\n");
-//Console.WriteLine($"Day 6:\n  Part 1: {Day6.Part1()}\n  Part 2: {Day6.Part2()}\n");
+﻿Console.WriteLine($"Solutions: ");
+Console.WriteLine($"Day 1:\n  Part 1: {Day1.Part1()}\n  Part 2: {Day1.Part2()}\n");
+Console.WriteLine($"Day 2:\n  Part 1: {Day2.Part1()}\n  Part 2: {Day2.Part2()}\n");
+Console.WriteLine($"Day 3:\n  Part 1: {Day3.Part1()}\n  Part 2: {Day3.Part2()}\n");
+Console.WriteLine($"Day 4:\n  Part 1: {Day4.Part1()}\n  Part 2: {Day4.Part2()}\n");
+Console.WriteLine($"Day 5:\n  Part 1: {Day5.Part1()}\n  Part 2: {Day5.Part2()}\n");
+Console.WriteLine($"Day 6:\n  Part 1: {Day6.Part1()}\n  Part 2: {Day6.Part2()}\n");
 Console.WriteLine($"Day 7:\n  Part 1: {Day7.Part1()}\n  Part 2: {Day7.Part2()}\n");
+Console.WriteLine($"Day 8:\n  Part 1: {Day8.Part1()}\n  Part 2: {Day8.Part2()}\n");
---
+++ Solutions/Day8.cs
@@ -0,0 +1,122 @@
+﻿public static class Day8
+{
+    public static int Part1()
+    {
+        var lines = File.ReadAllLines("./Inputs/day8-1.txt");
+        HashSet<Point> visible = new();
+
+        
+        // Check from left and right
+        foreach (var (y, line) in lines.Select((item, i) => (i, item)))
+        {
+            char tallest = (char)('0' - 1);
+
+            for (int x = 0; x < line.Length; x++)
+            {
+                if (line[x] > tallest)
+                {
+                    visible.Add(new Point(x, y));
+                    tallest = line[x];
+                }
+            }
+
+            tallest = (char)('0' - 1);
+
+            for (int x = line.Length - 1; x >= 0; x--)
+            {
+
+                if (line[x] > tallest)
+                {
+                    visible.Add(new Point(x, y));
+                    tallest = line[x];
+                }
+            }
+        }
+
+        // Now from top and bottom
+        for (int x = 0; x < lines[0].Length; x++)
+        {
+            char tallest = (char)('0' - 1);
+
+            for (int y = 0; y < lines.Length; y++)
+            {
+
+                if (lines[y][x] > tallest)
+                {
+                    visible.Add(new Point(x, y));
+                    tallest = lines[y][x];
+
+                }
+            }
+
+            tallest = (char)('0' - 1);
+
+            for (int y = lines.Length - 1; y >= 0; y--)
+            {
+                if (lines[y][x] > tallest)
+                {
+                    visible.Add(new Point(x, y));
+                    tallest = lines[y][x];
+                }
+            }
+        }
+
+        return visible.Count;
+    }
+
+    public static int Part2()
+    {
+        var lines = File.ReadAllLines("./Inputs/day8-1.txt");
+        int score = 0;
+        Point[] offsets = new Point[]
+        {
+            new(-1, 0), new (1, 0),
+            new(0, -1), new (0, 1)
+        };
+
+        for (int x = 0; x < lines[0].Length; x++)
+        {
+            for (int y = 0; y < lines.Length; y++)
+            {
+                int cellScore = 1;
+                char tallest = lines[y][x];
+
+                foreach (var offset in offsets)
+                {
+                    int d = 0;
+                    while (TryGetPoint(lines, x + offset.X * (d + 1), y + offset.Y * (d + 1), out char c))
+                    {
+                        d++;
+
+                        if (c >= tallest)
+                        {
+                            break;
+                        }
+                    }
+                    
+                    cellScore *= d;
+                }
+
+                if (cellScore > score)
+                {
+                    score = cellScore;
+                }
+            }
+        }
+
+        return score;
+    }
+
+    static bool TryGetPoint(string[] lines, int x, int y, out char c)
+    {
+        if (x < 0 || x >= lines.Length || y < 0 || y >= lines[0].Length) 
+        { 
+            c = '\0'; 
+            return false; 
+        }
+        c = lines[y][x];
+        return true;
+    }
+
+    record struct Point(int X, int Y);
+}
